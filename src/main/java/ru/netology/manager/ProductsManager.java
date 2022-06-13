c68181f5d907229ec68e3884fc9b6c4f18ad7b17
YuliaKreydunova <kreydunovay@gmail.com>
2022-06-13 13:42:05 +0500

    Change method matches

--- src/main/java/ru/netology/manager/ProductsManager.java
+++ src/main/java/ru/netology/manager/ProductsManager.java
@@ -17,7 +17,7 @@ public class ProductsManager {
     public Product[] searchBy(String text) {
         Product[] result = new Product[0];
         for (Product product : repository.findAll()) {
-            if (matches(product, text)) {
+            if (product.matches(text)) {
                 Product[] tmp = new Product[result.length + 1];
                 for (int i = 0; i < result.length; i++) {
                     tmp[i] = result[i];
@@ -29,12 +29,12 @@ public class ProductsManager {
         return result;
     }
 
-    public boolean matches(Product product, String search) {
-        if (product.getName().contains(search)) {
-            return true;
-        } else {
-            return false;
-        }
-//         return product.getName().contains(search);
-    }
+//    public boolean matches(Product product, String search) {
+//        if (product.getName().contains(search)) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+////         return product.getName().contains(search);
+//    }
 }
--- src/test/java/ru/netology/manager/ProductsManagerTest.java
+++ src/test/java/ru/netology/manager/ProductsManagerTest.java
@@ -8,10 +8,26 @@ import ru.netology.domain.Smartphone;
 import ru.netology.repository.ProductsRepository;
 
 public class ProductsManagerTest {
-    Book book1 = new Book(1, "Ulyssesung", 700, "James Joyce");
-    Smartphone smartphone1 = new Smartphone(2, "Samsung", 20000, "Samsung");
-    Book book2 = new Book(3, "The Invisible Man", 800, "");
+    Book book1 = new Book(1, "Ulyssesung123", 700, "James Joyce");
+    Smartphone smartphone1 = new Smartphone(2, "Samsung", 20000, "Samsung123");
+    Book book2 = new Book(3, "The Invisible Man1", 800, "Herbert Wells123");
 
+
+    @Test
+    public void test() {
+        ProductsRepository repository = new ProductsRepository();
+        ProductsManager manager = new ProductsManager(repository);
+
+        manager.add(book1);
+        manager.add(smartphone1);
+        manager.add(book2);
+
+        Product[] actual = manager.searchBy("123");
+        Product[] expected = {book1, smartphone1, book2};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
     @Test
     public void testSearchByTwoElements() {
         ProductsRepository repository = new ProductsRepository();
