3ba16a65dd40155b86c40d692c2216804bb3962a
YuliaKreydunova <kreydunovay@gmail.com>
2022-06-15 16:37:05 +0500

    added id matching check

--- src/main/java/ru/netology/repository/ProductsRepository.java
+++ src/main/java/ru/netology/repository/ProductsRepository.java
@@ -6,6 +6,11 @@ public class ProductsRepository {
     private Product[] products = new Product[0];
 
     public void save(Product product) {
+        for (Product product1 : products) {
+            if (product.getId() == product1.getId()) {
+                throw new AlreadyExistsException("Элемент с данным id уже существует");
+            }
+        }
         Product[] tmp = new Product[products.length + 1];
 
         System.arraycopy(products, 0, tmp, 0, products.length);
--- src/test/java/ru/netology/manager/ProductsManagerTest.java
+++ src/test/java/ru/netology/manager/ProductsManagerTest.java
@@ -1,18 +1,49 @@
 package ru.netology.manager;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
 import ru.netology.domain.Smartphone;
+import ru.netology.repository.AlreadyExistsException;
 import ru.netology.repository.NotFoundException;
 import ru.netology.repository.ProductsRepository;
 
 public class ProductsManagerTest {
     Book book1 = new Book(1, "Ulyssesung", 700, "James Joyce");
     Smartphone smartphone1 = new Smartphone(2, "Samsung", 20000, "Samsung");
-    Book book2 = new Book(3, "The Invisible Man", 800, "");
+    Book book2 = new Book(3, "The Invisible Man", 800, "H. G. Wellsn");
+    Book book3 = new Book(3, "The Invisible Man2", 900, "H. G. Wellsn");
 
+    @Test
+    public void testSaveExistingId() {
+        ProductsRepository repository = new ProductsRepository();
+        ProductsManager manager = new ProductsManager(repository);
+
+        manager.add(book1);
+        manager.add(smartphone1);
+        manager.add(book2);
+
+        assertThrows(AlreadyExistsException.class, () -> {
+            manager.add(book3);
+        });
+    }
+
+    @Test
+    public void testSave() {
+        ProductsRepository repository = new ProductsRepository();
+        ProductsManager manager = new ProductsManager(repository);
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(smartphone1);
+
+        Product[] actual = repository.findAll();
+        Product[] expected = {book1, book2, smartphone1};
+
+        assertArrayEquals(expected, actual);
+    }
 
     @Test
     public void testRemoveExistingElement() {
@@ -86,20 +117,6 @@ public class ProductsManagerTest {
 
         Product[] actual = manager.searchBy("n");
         Product[] expected = {book1, smartphone1, book2};
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void testFindAll() {
-        ProductsRepository repository = new ProductsRepository();
-        ProductsManager manager = new ProductsManager(repository);
-
-        manager.add(book1);
-        manager.add(book2);
-
-        Product[] actual = repository.findAll();
-        Product[] expected = {book1, book2};
 
         assertArrayEquals(expected, actual);
     }
